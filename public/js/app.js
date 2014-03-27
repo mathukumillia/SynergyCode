52426fec0845568c3c9add734a00a231d7677baf
pricemc <price.mc1x@gmail.com>
2014-03-27 10:54:15 -0600

    for the build

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'mean.system', 'mean.articles', 'ui.codemirror']);
+angular.module('mean', ['ngCookies', 'ui.router', 'ngResource', 'ui.bootstrap', 'mean.system', 'mean.articles', 'ui.codemirror']);
 
 angular.module('mean.system', []);
 angular.module('mean.articles', []);
