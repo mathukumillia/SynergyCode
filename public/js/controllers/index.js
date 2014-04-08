6a05c4720e318ff032cad50c33ca9b2516271683
Anirudh <mathukumillia@s.dcsdk12.org>
2014-04-08 16:20:34 -0600

    angular routing to file editing page

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -2,4 +2,11 @@
 
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
+
+    $scope.codeMirrorLoaded = function(editor){
+        editor.setOption('lineNumbers', true);
+        editor.setOption('theme', 'monokai');
+        editor.setOption('lineWrapping', true);
+        editor.setOption('mode', 'application/xml');
+    };
 }]);
