cfd20b50895ab7c3750dbc7cedbeac17e11cb9f3
Anirudh <mathukumillia@s.dcsdk12.org>
2014-05-17 19:20:00 -0600

    Live Preview supports HTML

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mean', ['ngCookies', 'ui.router', 'ngResource', 'ui.bootstrap', 'mean.system', 'mean.articles', 'ui.codemirror']);
+angular.module('mean', ['ngCookies', 'ui.router', 'ngResource', 'ui.bootstrap', 'mean.system', 'mean.articles', 'ui.codemirror', 'ngSanitize']);
 
 angular.module('mean.system', []);
 angular.module('mean.articles', []);
